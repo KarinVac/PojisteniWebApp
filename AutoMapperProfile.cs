69fdc3624cfee4dd375dbf330efc92820b7ae060
Karin Vaculíková <karin.vaculikova@seznam.cz>
2025-09-08 15:17:57 +0200

    Úprava layoutu - zarovnání hlavičky s hlavním obsahem stránky

--- AutoMapperProfile.cs
+++ AutoMapperProfile.cs
@@ -3,7 +3,7 @@ using PojisteniWebApp.Models;
 
 namespace PojisteniWebApp
 {
-    // V této třídě definujeme všechna pravidla pro "překlad" mezi databázovými modely a ViewModely (DTO).
+    // překlad mezi databázovými modely a ViewModely (DTO).
     public class AutoMapperProfile : Profile
     {
         public AutoMapperProfile()
